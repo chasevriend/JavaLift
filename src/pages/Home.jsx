f5596bbe9be33dc78b9e708d3ac376df4a287f2e
NEWUSER <chasevriend84@gmail.com>
2023-07-10 17:46:22 -0700

    revamp

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Nav from '../components/Nav'
+
+const Home = () => {
+  return (
+    <div>
+        <Nav />
+        <Hero />
+        {/* Programs
+
+        About us 
+
+        Reviews
+
+        Pricing
+
+        Call to Action
+
+        Footer */}
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/ProgramPg.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProgramPg = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default ProgramPg
