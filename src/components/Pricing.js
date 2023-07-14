4574fd63da0f4ec7c7293efb453e8bef6a05323c
NEWUSER <chasevriend84@gmail.com>
2023-07-14 07:45:34 -0700

    working on pricing component

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -6,11 +6,93 @@ const Pricing = () => {
         <section id='pricing' className='pricing'>
             <div className='pricing__header'>
                 <h2>Membership Plans</h2>
-                <p>JavaLift offers competitive pricing options for its services ensuring you can choose a plan 
-                that fits your budget and provides excellent value.</p>
+                <p>
+                    JavaLift offers competitive pricing options for its services ensuring you can choose a plan 
+                    that fits your budget and provides excellent value.
+                </p>
             </div>
             <div className='pricing__container'>
-
+                <div className='pricing__cell'>
+                    <p className='pricing__head'>Basic Plan</p>
+                    <h3>FREE</h3>
+                    <p>per month</p>
+                    <ul>
+                        <li>
+                            &#10003;
+                            1 Training program
+                        </li>
+                        <li>
+                            &#10003;
+                            Free fitness consultation
+                        </li>
+                        <li>
+                            &#10003;
+                            Tracking PR's progression
+                        </li>
+                        <li>
+                            &#10003;
+                            Tracking Bodyweight's progression
+                        </li>
+                    </ul>
+                </div>
+                <div className='pricing__cell'>
+                    <p className='pricing__head'>Basic Plan</p>
+                    <h3>FREE</h3>
+                    <p>per month</p>
+                    <ul>
+                        <li>
+                            &#10003;
+                            2 training programs
+                        </li>
+                        <li>
+                            &#10003;
+                            Free fitness consultation
+                        </li>
+                        <li>
+                            &#10003;
+                            Tracking PR's progression
+                        </li>
+                        <li>
+                            &#10003;
+                            Tracking Bodyweight's progression
+                        </li>
+                        <li>
+                            &#10003;
+                            24 Hour contactable trainers
+                        </li>
+                    </ul>
+                </div>
+                <div className='pricing__cell'>
+                    <p className='pricing__head'>Basic Plan</p>
+                    <h3>FREE</h3>
+                    <p>per month</p>
+                    <ul>
+                        <li>
+                            &#10003;
+                            5 training programs
+                        </li>
+                        <li>
+                            &#10003;
+                            Free fitness consultation
+                        </li>
+                        <li>
+                            &#10003;
+                            Tracking PR's progression
+                        </li>
+                        <li>
+                            &#10003;
+                            Tracking Bodyweight's progression
+                        </li>
+                        <li>
+                            &#10003;
+                            24 hour online personal trainers
+                        </li>
+                        <li>
+                            &#10003;
+                            Customizable Programs
+                        </li>
+                    </ul>
+                </div>
             </div>
         </section>
     </>
