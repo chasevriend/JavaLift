fe19e8fbac8bc5af8de2d9d09a026638b9383c69
NEWUSER <chasevriend84@gmail.com>
2023-07-15 11:52:54 -0700

    starting search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Exercises.css'
 
 const Search = () => {
-  return (
+    const [search, setSearch] = useState('')
+
+    const handleSearch = async () => {
+        if(search) {
+            const exercisesData = await fetchData()
+        }
+    }
+    
+    return (
     <>
         <div className='search'>
             <h1 className='search__header'>Exercise Library</h1>
             <div className='search__container'>
-                <input type='text' placeholder='Search Exercises' className='search__input' />
-                <button className='search__btn'>Search Library</button>
+                <input 
+                    type='text' 
+                    placeholder='Search Exercises' 
+                    className='search__input'
+                    onChange={(e) => setSearch(e.target.value.toLowerCase())} 
+                />
+                <button className='search__btn' onClick={handleSearch}>Search</button>
             </div>
         </div>
     </>
