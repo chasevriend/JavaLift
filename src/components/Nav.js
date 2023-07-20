a4bcbf9f00dac389373dc47564b4dea811ccefc7
NEWUSER <chasevriend84@gmail.com>
2023-07-20 08:23:27 -0700

    making javalift mobile responsive

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,6 +8,45 @@ const Nav = () => {
     const openNav = () => {
         setNav(!nav);
     }
+
+    const mobilePrograms = () => {
+        openNav();
+        document
+        .querySelector('#programs')
+        .scrollIntoView({ behavior: "smooth" });
+    }
+
+    const openPrograms = () => {
+        document
+        .querySelector('#programs')
+        .scrollIntoView({ behavior: "smooth" });
+    }
+
+    const openAbout = () => {
+        document
+        .querySelector('#about')
+        .scrollIntoView({ behavior: "smooth" });
+    }
+
+    const mobileAbout = () => {
+        openNav();
+        document
+        .querySelector('#about')
+        .scrollIntoView({ behavior: "smooth" });
+    }
+
+    const openPricing = () => {
+        document
+        .querySelector('#pricing')
+        .scrollIntoView({ behavior: "smooth" });
+    }
+
+    const mobilePricing = () => {
+        openNav();
+        document
+        .querySelector('#pricing')
+        .scrollIntoView({ behavior: "smooth" });
+    }
     
     return(
         <>
@@ -18,62 +57,50 @@ const Nav = () => {
                     </div>
                     <ul className="mobile-nav__links">
                         <li>
-                            <Link onClick={openNav} to="/">
-                                Home
+                            <Link onClick={mobilePrograms} to="/">
+                                Programs
                             </Link>
                         </li>
                         <li>
-                            <Link onClick={openNav} to="/about">
+                            <Link onClick={mobileAbout} to="/">
                                 About
                             </Link>
                         </li>
                         <li>
-                            <Link onClick={openNav} to="/models">
-                                Models
-                            </Link>
-                        </li>
-                        <li>
-                            <Link onClick={openNav} to="/testimonials">
-                                Testimonials
-                            </Link>
-                        </li>
-                        <li>
-                            <Link onClick={openNav} to="/">
-                                Contact
+                            <Link onClick={mobilePricing} to="/">
+                                Pricing
                             </Link>
                         </li>
                     </ul>
                 </div>
                 <div className="nav">
                     <div className="nav__left">
-                        <Link to="/" id="nav__logo" onClick={() => window.scrollTo(0, 0)}>
+                        <Link to="/" id="nav__logo">
                             JavaLift
                         </Link>
                     </div>
                     <ul className="nav__links">
                         <li>
-                            <Link className="nav__link" to="/">
-                                Home
-                            </Link>
-                        </li>
-                        <li>
-                            <Link className="nav__link" to="/">
+                            <Link className="nav__link" onClick={openPrograms}>
                                 Programs
                             </Link>
                         </li>
                         <li>
-                            <Link className="nav__link" to="/about">
+                            <Link className="nav__link" onClick={openAbout}>
                                 About
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className="nav__link" onClick={openPricing}>
+                                Pricing
                             </Link>
                         </li>
                     </ul>
                     <div className="nav__btns">
-                        <Link className="navBtn__exercise" to="/LibraryPg">
-                            Exercises
+                        <Link className="navBtn__exercise">
+                            Get Started
                         </Link>
-                        <Link className="navBtn__start" to="/">
-                            Start Now
-                        </Link>
+
                     </div>
                     <div className="mobile-nav__hamburger" onClick={openNav}>
                         <svg viewBox="0 0 100 80" width="40" height="40" className="mobile-nav__hamburgerIcon">
